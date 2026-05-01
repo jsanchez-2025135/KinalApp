41e2ede98c00960873522652f8daf96c46a59810
jsanchez-2025135 <jsanchez-2025135@kinal.edu.gt>
2026-04-30 23:50:05 -0600

    feat: ajusté configuración y servicio para Spring Security

--- src/main/java/com/jesussanchez/kinalapp/service/UserDetailsServiceImpl.java
+++ src/main/java/com/jesussanchez/kinalapp/service/UserDetailsServiceImpl.java
@@ -1,4 +1,22 @@
 package com.jesussanchez.kinalapp.service;
 
-public class UserDetailsServiceImpl {
+import com.jesussanchez.kinalapp.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // Buscamos por email que es lo que pide el login
+        return usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con email: " + email));
+    }
 }
